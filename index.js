0b93d3758bbb31ab3a88f4309fdf12f3a4781a5d
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-03-24 05:05:21 -0500

    finish exercise

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require("node:http");
+const fs = require("node:fs");
+const { stringify } = require("node:querystring");
+
+http
+  .createServer((request, response) => {
+    response.setHeader("Content-Type", "text/html");
+
+    let path = "./";
+
+    switch (request.url) {
+      case "/":
+        path += "index.html";
+        break;
+      case "/about":
+        path += "about.html";
+        break;
+      case "/contact-me":
+        path += "contact-me.html";
+        break;
+      default:
+        path += "404.html";
+        break;
+    }
+
+    fs.readFile(path, (error, dataRead) => {
+      error
+        ? (response.end("error"), console.error("Server error"))
+        : response.end(dataRead);
+    });
+  })
+  .listen(8080);
