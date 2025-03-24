b14bc7b440f7cac466fc539d486a010baaac7446
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-03-24 06:02:57 -0500

    update exercise

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
-const http = require("node:http");
-const fs = require("node:fs");
+// Updated project/
+const express = require("express");
+const app = express();
 
-http
-  .createServer((request, response) => {
-    response.setHeader("Content-Type", "text/html");
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
 
-    let path = "./";
+app.get("/about", (resquest, response) => {
+  response.sendFile(__dirname + "/about.html");
+});
 
-    switch (request.url) {
-      case "/":
-        path += "index.html";
-        break;
-      case "/about":
-        path += "about.html";
-        break;
-      case "/contact-me":
-        path += "contact-me.html";
-        break;
-      default:
-        path += "404.html";
-        break;
-    }
+app.get("/contact-me", (resquest, response) => {
+  response.sendFile(__dirname + "/contact-me.html");
+});
 
-    fs.readFile(path, (error, dataRead) => {
-      error
-        ? (response.end("error"), console.error("Server error"))
-        : response.end(dataRead);
-    });
-  })
-  .listen(8080);
+app.use((resquest, response) => {
+  response.status(404).sendFile(__dirname + "/404.html");
+});
+
+app.listen(8080);
+
+// Original Project/
+
+// const http = require("node:http");
+// const fs = require("node:fs");
+
+// http
+//   .createServer((request, response) => {
+//     response.setHeader("Content-Type", "text/html");
+
+//     let path = "./";
+
+//     switch (request.url) {
+//       case "/":
+//         path += "index.html";
+//         break;
+//       case "/about":
+//         path += "about.html";
+//         break;
+//       case "/contact-me":
+//         path += "contact-me.html";
+//         break;
+//       default:
+//         path += "404.html";
+//         break;
+//     }
+
+//     fs.readFile(path, (error, dataRead) => {
+//       error
+//         ? (response.end("error"), console.error("Server error"))
+//         : response.end(dataRead);
+//     });
+//   })
+//   .listen(8080);
